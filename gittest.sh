d73475a10c594133db4ec28035b30cd10e813cc5
weisenhao <weisenhao@58.com>
2024-04-25 14:34:36 +0800

    no message

--- gittest.sh
+++ gittest.sh
@@ -1,7 +1,54 @@
+# key="\-ld64"
+# grep  ${key} 58.html 
+# if [ $? -ne 0 ]; then  
+# echo fail
+# else
+# echo ok
+# fi
+
+# hook_dir = '.git/hooks/'
+# cd hook_dir
+
+
+
+
+
 key="\-ld64"
-grep  ${key} 58.html 
+
+cd '.git/hooks/'
+
+if test -e ok; then
+    echo "文件存在"
+else
+touch ok
+echo "文件不存在"
+fi
+
+
+
+grep  -i ${key} ok 
 if [ $? -ne 0 ]; then  
+echo 未安装
+
+cat >> ok <<EOF
+
+grep  '\-ld64' 58.html 
+if [ \$? -ne 0 ]; then  
 echo fail
 else
 echo ok
 fi
+EOF
+
+echo 安装完成
+else
+
+echo 已安装
+
+fi
+
+chmod 777 ok
+
+exit 0
+
+
